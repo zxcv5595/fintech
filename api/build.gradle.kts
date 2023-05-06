4937785841a0d9646f7ea8c8ef7df659bbcbddd3
zxcv5595 <zxcv5595@korea.ac.kr>
2023-05-06 21:01:18 +0900

    Apply swagger for api doc

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -7,5 +7,6 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.6")
+    implementation("io.springfox:springfox-boot-starter:3.0.0")
     implementation(project(":domain"))
 }
